e0d80b999f2abf484b761fa41869f9248dfca68c
Marielle Dado <mdado@DESKTOP-VQHE3PT.localdomain>
2020-06-07 19:45:59 +0200

    updated README, added minimal package structure

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ds-lab
+# WiMLDS Berlin Data Science Lab
+
+This repository contains project files for the first cohort of WiMLDS Berlin Data Science Lab. 
+
+
+
+## Setup Workflow
+Note: The commands in this README work on Unix Bash command line.
+
+1. Clone or download this repo
+`git clone https://github.com/izzbizz/ds-lab.git`
+2. Change working directory to root folder of the project
+`cd ds-lab`
+3. Make sure you have Anaconda/`conda` installed. Verify using the command `conda --version`.
+4. Create a new virtual environment named `dslab`
+`conda create --name dslab python=3.7`
+5. Activate virtual environment `dslab`
+`conda activate dslab`
+6. Install `requirements.txt`
+`pip install -r requirements.txt`
---
+++ setup.py
@@ -0,0 +1,29 @@
+  
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='emissionscheck_alb',
+    version='0.1',
+    description='Early Warning System for Predicting Polluting Vehicles in Albuquerque',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    # Substitute <github_account> with the name of your GitHub account
+    url='https://github.com/izzbizz/ds-lab',
+    author='WiMLDS Berlin Data Science Lab Cohort #1',  # Substitute your name
+    author_email='berlin@wimlds.org',  # Substitute your email
+    license='MIT',
+    packages=['emissionscheck_alb'],
+)
