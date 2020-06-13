a1e26e219b7b04336a3516707e3b076c57e7db5c
Marielle Dado <mdado@DESKTOP-VQHE3PT.localdomain>
2020-06-13 20:56:46 +0200

    modified script based on Isabelle's feedback

--- emissionscheck_alb/baseline_model.py
+++ emissionscheck_alb/baseline_model.py
@@ -1,59 +1,75 @@
 import pandas as pd
 import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import roc_auc_score, plot_confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 
 def run_baseline_model(features_train, features_test, target_train, target_test, model, random_state=None, class_weight=None, max_fpr=None):
     """
-    Run logistic regression or SVC  
-    
+    Run logistic regression or SVC with cross validation
+
+    Parameters
+    ----------
+    features_train: numpy.ndarray
+
+    features_test: numpy.ndarray
+
+    target_train: numpy.ndarray
+
+    target_test: numpy.ndarray
+
+    model: str, "logreg" or "svc"
+        Run logistic regression or support vector classifier
+
+    random_state : int
+        Pass an int for reproducible output across multiple function calls.
+
+    class_weight : None or "balanced"
+        Adjust weights inversely proportional to class frequencies
+
+    max_fpr: float > 0 and <= 1
+        Standardized partial AUC over the range [0, max_fpr] is given
+
+    Notes
+    -----
+    Shows confusion matrix, classification report, cross val score (ROC AUC)
+
     """
-    from sklearn.pipeline import make_pipeline
-    from sklearn.preprocessing import MinMaxScaler
-    from sklearn.metrics import roc_auc_score, plot_confusion_matrix, classification_report
-    from sklearn.model_selection import cross_val_score
 
     if model == "logreg":
-
-        from sklearn.linear_model import LogisticRegression
         lr = LogisticRegression(class_weight=class_weight, random_state=random_state)
 
         clf_pipeline = make_pipeline(MinMaxScaler(), lr)
 
     elif model == "svc":
-
-
-        from sklearn.svm import SVC
         svc = SVC(class_weight=class_weight)
 
         clf_pipeline = make_pipeline(MinMaxScaler(), svc)
 
-
-    # fit model
+    # fit and predict labels and probabilities
     fitted_model = clf_pipeline.fit(features_train, target_train)
 
-    # predict labels
     target_pred = clf_pipeline.predict(features_test)
 
-    # predict predicted probabilities
     target_pred_proba = clf_pipeline.predict_proba(features_test)
 
-    # classification report
+    # compute metrics
     print(classification_report(target_test, target_pred))
 
-    # confusion matrix
     plot_confusion_matrix(clf_pipeline, features_test, target_test)
 
-    # calculate ROC
     roc_auc = roc_auc_score(target_test, target_pred_proba[:, 1], max_fpr=max_fpr)
     print(f'ROC AUC Score is {roc_auc}')
-    
-    # cross val score
+
+    # cross validation
     cv_score = cross_val_score(clf_pipeline, features_train, target_train, scoring="roc_auc")
-    
     mean_cv_score = round(np.mean(cv_score), 2)
-    
+
     print(f'Cross-validation scores: {cv_score}')
     print(f'Mean cross-validation score: {mean_cv_score}')
-
 
     return fitted_model
--- emissionscheck_alb/data_prep.py
+++ emissionscheck_alb/data_prep.py
@@ -1,23 +1,23 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def keep_pass_fail(data):
     '''
-    Removes rows that do not have value `"P"` or `"F"` in target variable `OVERALL_RESULT`
-    
+    Removes rows that don't have pass/fail target variable in `OVERALL_RESULT`
+
     Parameters
     ----------
-    df : pandas.DataFrame
-        Data-frame containing the column `OVERALL_RESULT`
+    df : pandas.DataFrame containing column `OVERALL_RESULT`
+
     Returns
     -------
     pandas.DataFrame
-        DataFrame of rows with desired values `'P'` or `'F'` in target variable 
     '''
-    # Mask to select only rows with desired target values
-    overall_result_mask = (data['OVERALL_RESULT'] == 'P') | (data['OVERALL_RESULT'] == 'F')
-    
-    return data[overall_result_mask]
+    data = data[data["OVERALL_RESULT"].isin(['P', 'F'])]
+
+    return data
+
 
 def get_car_age(data):
     '''
@@ -25,32 +25,25 @@ def get_car_age(data):
 
     Parameters
     ----------
-    df : pandas.DataFrame
-        DataFrame containing the columns `TEST_EDATE` and `'MODEL_YEAR'`
-        `'MODEL_YEAR'` should be `int` type
+    data : pandas.DataFrame
+        Contains columns `TEST_EDATE` (datetime) and `'MODEL_YEAR'` (int)
 
     Returns
     -------
     pandas.DataFrame
-        DataFrame of rows with new column `CAR_AGE`
-
-
-    Notes
-    -----
-    Adding +1 to car age to avoid negative values (many cars are released a year prior to year in car model)
+        DataFrame with new column `CAR_AGE`
 
     '''
-    # make copy of df
+    # make copy of df to prevent SettingWithCopyWarning
     df_copy = data.copy()
 
-    # convert df['TEST_EDATE'] to datetime
+    df_copy['CAR_AGE'] = pd.to_datetime(df_copy['TEST_EDATE']).dt.year - df_copy['MODEL_YEAR']
 
-    df_copy.loc[:, 'CAR_AGE'] = pd.to_datetime(df_copy.loc[: ,'TEST_EDATE']).dt.year - df_copy.loc[:, 'MODEL_YEAR']
-
-    # add +1 to df['CAR_AGE']
-    df_copy.loc[:, 'CAR_AGE'] += 1
+    # add +1 to avoid negative age values
+    df_copy['CAR_AGE'] += 1
 
     return df_copy
+
 
 def encode_target(target_series):
     '''
@@ -58,16 +51,10 @@ def encode_target(target_series):
 
     Parameters
     ----------
-    df : pandas.DataFrame
-        Data-frame containing the column `OVERALL_RESULT`
-    Returns
-    -------
-    pandas.DataFrame
-        DataFrame with encoded `F` and `P` target variable 
-    '''
-    # make copy of df
+    target_series : pandas.Series
 
-    # Mask to select only rows with desired target values
+    '''
+
     target_series = target_series.replace({'P': 0, 'F': 1})
 
     return target_series
@@ -76,28 +63,27 @@ def encode_target(target_series):
 def split_data(features_matrix, target_series, train_size=None, random_state=None):
     """
     Split dataset into training and test sets
-    
+
     Parameters
     ----------
     features_matrix : pandas.DataFrame
         Features/predictors of the dataset
-    
+
     target_series : pandas Series
         Observation labels to be predicted
 
-    train_size : float
-        The proportion of the dataset to include in the train split. Should be between 0.0 and 1.0
-        
+    train_size : float (between 0.0 - 1.0)
+        The proportion of dataset to include in the train split
+
     random_state : int
         Pass an int for reproducible output across multiple function calls.
-    
+
     Returns
     -------
     tuple of numpy.ndarray,
         features_train, features_test, target_train, target_test
-        
+
     """
-    from sklearn.model_selection import train_test_split
 
     features_train, features_test, target_train, target_test = train_test_split(
         features_matrix,
@@ -108,43 +94,41 @@ def split_data(features_matrix, target_series, train_size=None, random_state=Non
 
     return features_train, features_test, target_train, target_test
 
-def encode_split_data(cat_features, cont_features, data, target_series, train_size, random_state=None):
+def encode_split_data(cat_features, data, target_series, train_size, random_state=None):
     """
     Encodes categorical features, concatenates encoded features with continuous features and splits data
-    
+
     Parameters
     ----------
     cat_features : list of str
         List of column names in `data` containing categorical features
-    
-    cont_features : list of str
-        List of column names in `data` containing continuous features
-    
+
     data : pandas.DataFrame
 
-                
     target_series : pandas Series
         Observation labels to be predicted
 
-    train_size : float
-        The proportion of the dataset to include in the train split. Should be between 0.0 and 1.0
-        
+    train_size : float (between 0.0 - 1.0)
+        The proportion of dataset to include in the train split
+
     random_state : int
         Pass an int for reproducible output across multiple function calls.
-    
+
     Returns
     -------
     tuple of numpy.ndarray,
         features_train, features_test, target_train, target_test
-        
+
     """
     df_working_copy = data.copy()
-    
+
     # convert categorical columns into `category` dtype
-    for column_name in df_working_copy[cat_features].columns:
+    for column_name in cat_features:
         df_working_copy[column_name] = df_working_copy[column_name].astype("category")
 
     features_dummy = pd.get_dummies(df_working_copy[cat_features], drop_first=True)
+
+    cont_features = [col for col in data.columns if not col in cat_features]
 
     features_encoded = pd.concat([df_working_copy[cont_features], features_dummy], axis=1)
 
@@ -158,4 +142,3 @@ def encode_split_data(cat_features, cont_features, data, target_series, train_si
     )
 
     return features_train, features_test, target_train, target_test
-
